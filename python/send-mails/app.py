ed72cf534460977950650961dea828cf308a7b37
Mateus Fernandes Santos <ferrmathh2@gmail.com>
2022-08-10 18:51:59 -0300

    Fix: increment logic

--- python/send-mails/app.py
+++ python/send-mails/app.py
@@ -1,6 +1,29 @@
 import os
 from dotenv import load_dotenv
-from email.message import EmailMessage
 
 load_dotenv()
 
+from conectServer import getServer
+from openFile import openExcel
+from createEmail import createEmail
+from certificateGenerate import certificateGenerate
+
+
+def app():
+    table = openExcel(fileName="aprovados.xlsx")
+    server = getServer()
+
+    i = 0
+    for aprovedEmail in table["E-mail"]:
+        if str(table["Nome completo"][i]).__contains__("Rita"):
+            templateName = "assinatura_cristiana.jpg"
+        elif str(table["Nome completo"][i]).__contains__("Cristiana"):
+            templateName = "assinatura_rita.jpg"
+        else:
+            templateName = "duas_assinaturas.jpg"
+        pdfName = certificateGenerate(
+            name= table["Nome completo"][i],
+            count= i,
+            templateName=templateName,
+            text= "",
+        )
