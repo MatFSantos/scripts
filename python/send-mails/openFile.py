e3a765e5271fe8034f8ec266e367039d3941c2ce
Mateus Fernandes Santos <ferrmathh2@gmail.com>
2022-08-10 18:50:12 -0300

    Feat: new method to open a excel file

---
+++ python/send-mails/openFile.py
@@ -0,0 +1,7 @@
+import pandas
+
+def openExcel(fileName: str)-> pandas.DataFrame:
+    print("Abrindo o excel "+ fileName)
+    table = pandas.read_excel("planilhas/"+fileName)
+    print("Excel "+ fileName + "aberto")
+    return table
